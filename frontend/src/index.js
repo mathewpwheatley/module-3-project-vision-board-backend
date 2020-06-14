2e8a70371a8c30c2c753fcad1241b9f99e07de5e
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-06-13 22:44:28 -0700

    Add DOM load event listener

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,3 +2,7 @@ const BASE_URL = "http://localhost:3000"
 const USERS_URL = `${BASE_URL}/users`
 const BOARDS_URL = `${BASE_URL}/boards`
 const GOALS_URL = `${BASE_URL}/goals`
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+  })
