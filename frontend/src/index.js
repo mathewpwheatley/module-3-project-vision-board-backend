a972d61277ddb358aad1ba240fcbf2f43a5912eb
Colton <kaiserc2@seattleu.edu>
2020-06-15 17:41:31 -0700

    Creates notes, buttons, form, CRUD functionality, and cleans up CSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,111 @@
-const BASE_URL = "http://localhost:3000"
-const USERS_URL = `${BASE_URL}/users`
-const BOARDS_URL = `${BASE_URL}/boards`
-const GOALS_URL = `${BASE_URL}/goals`
+const BASE_URL = "http://localhost:3000";
+const USERS_URL = `${BASE_URL}/users`;
+const BOARDS_URL = `${BASE_URL}/boards`;
+const GOALS_URL = `${BASE_URL}/goals`;
 
-document.addEventListener("DOMContentLoaded", function() {
-    
-  })
+document.addEventListener("DOMContentLoaded", function () {
+
+  const newGoalForm = document.getElementById("new-goal");
+  const goalFormLabel = document.getElementById("form-label")
+  const goalsSection = document.getElementById("notes");
+  const nameInput = document.getElementById("name")
+  const descriptionInput = document.getElementById("description")
+  const statusInput = document.getElementById("status")
+  const submitButton = document.getElementById("submit-button")
+
+  function createGoalCard(goal) {
+    const li = document.createElement("li");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    const h4 = document.createElement("h4");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    editButton.innerText = "Edit";
+    deleteButton.innerText = "Delete";
+    editButton.style.marginRight = "5px";
+    deleteButton.style.marginLeft = "5px";
+
+    goalsSection.appendChild(li);
+    li.appendChild(h2);
+    li.appendChild(p);
+    li.appendChild(h4);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+
+    li.setAttribute("goal-id", goal.id)
+    h2.innerHTML = goal.attributes.title;
+    p.innerHTML = goal.attributes.content;
+    h4.innerHTML = goal.attributes.status;
+
+    deleteButton.addEventListener("click", function () {
+      li.remove()
+      return fetch(`${GOALS_URL}/${goal.id}`, {
+        method: 'DELETE'
+      })
+      .then(response => response.json()
+      .then(json => {
+        return json
+      }))
+    });
+
+    editButton.addEventListener("click", function() {
+      goalFormLabel.innerHTML = `<strong>Edit ${goal.attributes.title}</strong>`
+      nameInput.value = goal.attributes.title
+      descriptionInput.value = goal.attributes.content
+      statusInput.value = goal.attributes.status
+      submitButton.setAttribute("goal-id", goal.id)
+      submitButton.value = "Complete Edit"
+    })
+  }
+
+  function fetchGoals() {
+    fetch(GOALS_URL)
+      .then((response) => response.json())
+      .then((json) => {
+        json.data.forEach(function (goal) {
+          createGoalCard(goal)
+        }) 
+      });
+  }
+
+  function createOrEditGoal() {
+    newGoalForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      console.log(event)
+      const goal = {
+        // const id = event.target.
+        
+        title: `${event.target[0].value}`,
+        content: `${event.target[1].value}`,
+        status: `${event.target[2].value}`,
+      };
+      if (submitButton.value === "Complete Edit") {
+        console.log(goal.title)
+        let editedGoal = document.querySelector(`goal-id = ${goal.id}`)
+        editedGoal.remove()
+        createGoalCard(goal)
+        const data = {
+          title: goal.title,
+          content: goal.content,
+          status: goal.status
+        }
+        fetch(`${GOALS_URL}/${goal.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data)
+        })
+      }
+      else {
+      createGoalCard(goal);
+    }
+      newGoalForm.reset()
+      statusInput.value = "-- Select a Status --"
+    });
+  }
+
+  fetchGoals();
+  createOrEditGoal();
+});
